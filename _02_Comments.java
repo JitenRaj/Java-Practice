bc66927183ba8d57738f5c0a216e10685e765762
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-04-24 22:42:37 +0530

    Comments

---
+++ _02_Comments.java
@@ -0,0 +1,11 @@
+class comment {
+  public static void main(String[ ] args) {
+
+    // this is single line comment
+
+    /* this is multiline
+    comments */
+
+    System.out.println("comments in java");
+  }
+}
