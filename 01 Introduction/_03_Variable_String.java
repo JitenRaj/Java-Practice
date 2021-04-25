f36b838d17fada2da10fe01eb9f1466e31720fc4
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-04-25 23:30:27 +0530

    Variable : String

---
+++ 01 Introduction/_03_Variable_String.java
@@ -0,0 +1,8 @@
+public class _03_Variable_String {
+  public static void main(String[] args) {
+    String firstName = "John ";
+    String lastName = "Doe";
+    String fullName = firstName + lastName;
+    System.out.println(fullName);
+  }
+}
