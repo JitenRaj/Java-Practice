7781c33b1f29a40499e9879f7762c91f07056a87
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-04-25 23:31:02 +0530

    Data Types

---
+++ 01 Introduction/_05_DataTypes.java
@@ -0,0 +1,23 @@
+/* In Java, we have basically 5 types of Data,
+Here is the list of those Data Types :
+String = "Hello"; (text/word)
+int = 5; (whole number)
+float = 10.5; (floating number)
+char = 'a'; (single character)
+boolean = True/False;    (boolean values)         */
+
+public class _05_DataTypes {
+  public static void main(String[] args) {
+    int myNum = 5;               // Integer (whole number)
+    float myFloatNum = 5.99f;    // Floating point number
+    char myLetter = 'D';         // Character
+    boolean myBool = true;       // Boolean
+    String myText = "Hello";     // String
+
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  }
+}
