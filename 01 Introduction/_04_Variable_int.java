901520488fe827af02a6da162c68c03a7461d625
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-04-25 23:30:42 +0530

    Variable : int

---
+++ 01 Introduction/_04_Variable_int.java
@@ -0,0 +1,9 @@
+public class _04_Variable_int {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 6;
+    System.out.println(x + y); // Print the value of x + y
+    int a = 5, b = 6, c = 50;
+    System.out.println(a + b + c);
+  }
+}
