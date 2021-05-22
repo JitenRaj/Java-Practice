e2277272ef1f30eec851a1f6c59057089088a720
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-05-22 22:20:47 +0530

    math sqrt

---
+++ 02 Built-in Functions/_03_math_sqrt.java
@@ -0,0 +1,8 @@
+public class _03_math_sqrt {
+  public static void main(String[] args) {
+    int a = 64;
+    Double ans = Math.sqrt(a);
+    System.out.println(ans);
+    System.out.println(Math.sqrt(81));
+  }
+}
