30822f835cd8c6a368715ed27e8a8f4eb4d2a55b
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-05-22 22:30:25 +0530

    boolean

---
+++ 01 Introduction/_07_Boolean_type.java
@@ -0,0 +1,17 @@
+/**
+ * _07_Boolean_type
+ */
+public class _07_Boolean_type {
+  public static void main(String[] args) {
+    boolean isjavafun = true;
+    boolean isjavahard = false;
+
+    System.out.println(isjavafun);
+    System.out.println(isjavahard);
+
+    int x=9,y=10;
+    System.out.println(x>y);
+    System.out.println(x==y);
+    System.out.println(x<y);
+  }
+}
