54e2bab83ecb3b6ade1e25cf5e43ee5abfe3ff2d
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-05-23 23:58:21 +0530

    string CharAt

---
+++ 02 String Functions/_01_string_char_at.java
@@ -0,0 +1,13 @@
+public class _01_string_char_at {
+  public static void main(String[] args) {
+    String FirstName = "Jitendra Singh";
+    String LastName = "Rajpurohit";
+    char FirstNameInitial = FirstName.charAt(0);
+    char LastNameInitial = LastName.charAt(0);
+
+    System.out.println(FirstName);
+    System.out.println(FirstNameInitial);
+    System.out.println(LastName);
+    System.out.println(LastNameInitial);
+  }
+}
