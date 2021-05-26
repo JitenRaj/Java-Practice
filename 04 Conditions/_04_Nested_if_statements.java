7f69ffed92daf8c6a30b1692b9643b30ad7aedc8
Jitendra Singh Rajpurohit <Ji10raj@gmail.com>
2021-05-26 23:52:23 +0530

    Nested if else

---
+++ 04 Conditions/_04_Nested_if_statements.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class _04_Nested_if_statements {
+  public static void main(String[] args) {
+    int age;
+
+    System.out.println("Enter your age:");
+    Scanner agescan = new Scanner(System.in);
+    age = agescan.nextInt();
+
+    if (age > 0) {
+      if (age > 16) {
+        System.out.println("Welcome");
+      } else {
+        System.out.println("Too Young");
+      }
+    } else {
+      System.out.println("Error");
+    }
+  }
+}
