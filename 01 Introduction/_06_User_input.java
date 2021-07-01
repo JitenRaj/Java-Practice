f69fbfca183383c1171a297d74cdc97fa205b1a9
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-01 16:32:23 +0530

    User input

--- 01 Introduction/_06_User_input.java
+++ 01 Introduction/_06_User_input.java
@@ -6,7 +6,7 @@ public class _06_User_input {
   public static void main(String[] args) {
     System.out.println("enter a number :");
     Scanner myVar = new Scanner(System.in);
-    System.out.println(myVar.nextLine());
+    System.out.println("number entered is =" + myVar.nextLine());
     myVar.close();
   }
 }
