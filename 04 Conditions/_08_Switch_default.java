25450cc6b2b4df371a633ad59cc7d3c0b28a45d2
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-02 22:12:25 +0530

    Switch Default

---
+++ 04 Conditions/_08_Switch_default.java
@@ -0,0 +1,15 @@
+public class _08_Switch_default {
+    public static void main(String[] args) {
+        int day = 5;
+        switch (day) {
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+             System.out.println("Looking forward to the weekend");
+        }
+    }
+}
