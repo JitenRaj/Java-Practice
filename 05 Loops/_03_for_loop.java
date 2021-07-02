f66bd1529996f213d3449a5d354ad5391a9f894d
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-02 22:46:36 +0530

    For loop

---
+++ 05 Loops/_03_for_loop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class _03_for_loop {
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Enter a number: ");
+        Scanner num_scan = new Scanner(System.in);
+        num = num_scan.nextInt();
+
+        System.out.println("for loop: ");
+
+        for( int i = 0 ; i < num ; i++) {
+            System.out.println(i);
+        }
+    }
+}
