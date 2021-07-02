7b6bcc3acc02a7cda89376d63536df9c4f850d7e
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-02 22:57:41 +0530

    for ech loop

---
+++ 05 Loops/_04_for_each_loop.java
@@ -0,0 +1,10 @@
+// "for-each" loop , which is used exclusively to loop through elements in an array
+
+public class _04_for_each_loop {
+    public static void main(String[] args) {
+        String[] cars = {"maruti","suzuki","lamborghini","audi"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
