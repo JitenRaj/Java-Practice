cb23bf02630ca5c0af942f1d80322c20755a1dbd
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-02 22:23:27 +0530

    Do while

---
+++ 05 Loops/_02_do_while.java
@@ -0,0 +1,15 @@
+public class _02_do_while {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i<5);
+
+        int j = 6;
+        do {
+            System.out.println(j);
+            j++;
+        } while (j<5);
+    }
+}
