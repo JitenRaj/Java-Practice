69a81948a9a60d1bf645264338bac200e51f27b6
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-04 22:45:26 +0530

    fortune teller using if else

--- 04 Conditions/_06_fortune_teller.java
+++ 04 Conditions/_06_fortune_teller.java
@@ -6,11 +6,13 @@ public class _06_fortune_teller {
         Scanner input = new Scanner(System.in);
 
         int inputNum = input.nextInt();
+        
 
         if (inputNum < 5) {
             System.out.println("Enjoy the good luck a friend brings you");
         } else {
             System.out.println("You will win a lottery");
         }
+        input.close();
     }
 }
