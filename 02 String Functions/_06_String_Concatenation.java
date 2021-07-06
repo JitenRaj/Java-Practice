490fcd989a1c9e6ac7758e40470b544a5ad9f11a
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-06 23:31:52 +0530

    String Concatenation

---
+++ 02 String Functions/_06_String_Concatenation.java
@@ -0,0 +1,7 @@
+public class _06_String_Concatenation {
+    public static void main(String[] args) {
+        String firstName = "Jitendra Singh";
+        String lastName = "Rajpurohit";
+        System.out.println("My name is " + firstName + " " + lastName);
+    }
+}
---
+++ 02 String Functions/_07_String_Concatenation.java
@@ -0,0 +1,7 @@
+public class _07_String_Concatenation {
+    public static void main(String[] args) {
+        String firstName = "Jitendra Singh ";
+        String lastName = "Rajpurohit";
+        System.out.println(firstName.concat(lastName));
+    }
+}
