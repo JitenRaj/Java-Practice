cf6d8f7280cd058f00bcbde04d494d37b93a81c4
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-06 23:25:08 +0530

    indexOf a string

---
+++ 02 String Functions/_05_String_indexOf.java
@@ -0,0 +1,10 @@
+public class _05_String_indexOf {
+    public static void main(String[] args) {
+        String text = "Hello, i am jiten";
+        System.out.println(text.indexOf("Hello"));
+        System.out.println(text.indexOf(","));
+        System.out.println(text.indexOf("i"));
+        System.out.println(text.indexOf("am"));
+        System.out.println(text.indexOf("jiten"));
+    }
+}
