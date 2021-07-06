ef0257eec0c1f5b6664f56bf7f3326a6769da899
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-06 23:19:23 +0530

    string length

---
+++ 02 String Functions/_02_String_length.java
@@ -0,0 +1,6 @@
+public class _02_String_length {
+    public static void main(String[] args) {
+        String text = "qwertyuiop";
+        System.out.println("The lenght of the text sring is:" + text.length());
+    }
+}
