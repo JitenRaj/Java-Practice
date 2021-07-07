c69d74af2d9cf4b5e3b0036749123442916910df
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-07 23:59:45 +0530

    Access array element

---
+++ 06 Arrays/_02_Access_arrays_element.java
@@ -0,0 +1,12 @@
+/**
+ * _02_Access_arrays_element
+ */
+public class _02_Access_arrays_element {
+    public static void main(String[] args) {
+        //You access an array element by referring to the index number.
+
+        // This statement accesses the value of the first element in cars:
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars[0]);
+    }
+}
