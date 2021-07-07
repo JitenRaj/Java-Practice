dbe827b6863654e9d4f702a1f330c37d2808fb3f
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-08 00:05:57 +0530

    Array length

---
+++ 06 Arrays/_04_array_length.java
@@ -0,0 +1,11 @@
+public class _04_array_length {
+    public static void main(String[] jiten) {
+       // To find out how many elements an array has, use the length property:
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars.length);
+
+        String companies[] = {"Audi","Jaguar","Lamborghini"};
+        System.out.println(companies.length);
+    }
+}
