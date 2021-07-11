e0bb9ccc442910c687ceea6d109dac6ccff0b10c
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-11 23:15:04 +0530

    multiple parameter

---
+++ 07 Functions/_05_multiple_parameter.java
@@ -0,0 +1,11 @@
+public class _05_multiple_parameter {
+    static void myMethod(String fname, int age) {
+        System.out.println(fname + " is " + age + " years old");
+      }
+    
+    public static void main(String[] args) {
+        myMethod("Jitendra",21);
+        myMethod("Goutam",17);
+        myMethod("Rahul",10);
+      }
+}
