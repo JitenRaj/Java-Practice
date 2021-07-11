816ce315fcddaa32ffd78f1c344f9c2a01402f29
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-11 23:12:28 +0530

    Calling functions several times

---
+++ 07 Functions/_04_calling_functions_multiple_times.java
@@ -0,0 +1,11 @@
+public class _04_calling_functions_multiple_times {
+    static void myMethod(String fname) {
+        System.out.println(fname + " Singh");
+      }
+    
+    public static void main(String[] args) {
+        myMethod("Jitendra");
+        myMethod("Goutam");
+        myMethod("Rahul");
+      }
+}
