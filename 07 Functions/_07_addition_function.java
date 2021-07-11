228543ec854076b8c894afe0ac3933a855f90be4
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-11 23:20:29 +0530

    add method/function with two parameter

---
+++ 07 Functions/_07_addition_function.java
@@ -0,0 +1,11 @@
+public class _07_addition_function {
+    static int Add(int x, int y) {
+        return x + y;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(Add(5, 15));
+        System.out.println(Add(6, 34));
+        System.out.println(Add(8, 42));
+    }
+}
