8b8494db277c4e495f9fc50ef42641486a765da2
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-11 22:43:49 +0530

    Using For-Each : Loop through an Array

---
+++ 06 Arrays/_06_for_each_loop_Array.java
@@ -0,0 +1,11 @@
+/**
+ * _06_for_each_loop_Array
+ */
+public class _06_for_each_loop_Array {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
