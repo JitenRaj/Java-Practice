547004e219ed2a33bb15d80a10f91612f3d2d5b8
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-11 22:43:14 +0530

    Loop through an Array

---
+++ 06 Arrays/_05_Loop_through_an_Array.java
@@ -0,0 +1,11 @@
+/**
+ * _05_Loop_through_an_Array
+ */
+public class _05_Loop_through_an_Array {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+    }
+}
