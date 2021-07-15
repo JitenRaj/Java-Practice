4abb6717510b4018317ec275ab530c73f687bf13
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-15 22:00:35 +0530

    Class and Object Creation

---
+++ JV09_Classes/JV01_Second.java
@@ -0,0 +1,10 @@
+class JV01_Main {
+    int x = 5;
+    
+}
+public class JV01_Second {
+    public static void main(String[] args) {
+        JV01_Main myObject = new JV01_Main();
+        System.out.println(myObject.x);
+    }
+}
