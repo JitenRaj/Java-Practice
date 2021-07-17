fdbb85c78fd13116cefb61c98426ef406c3f8179
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-17 23:41:27 +0530

    Constructor

--- JV09_Classes/JV09_Constructor.java
+++ JV09_Classes/JV09_Constructor.java
@@ -7,7 +7,11 @@
 public class JV09_Constructor {
     public static void main(String[] args) {
         JV09_Triangle TriangleA = new JV09_Triangle(15,2,4,5,7);
-        double Area = TriangleA.findArea();
-        System.out.println(Area);
+        double Area1 = TriangleA.findArea();
+        System.out.println(Area1);
+
+        JV09_Triangle TriangleB = new JV09_Triangle(15,2,4,5,7);
+        double Area2 = TriangleB.findArea();
+        System.out.println(Area2);
     }
 }
