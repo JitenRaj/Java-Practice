95e41d3452d39ac884fc77a65ad568d618aff40f
Jitendra Singh Rajpurohit <ji10raj@gmail.com>
2021-07-21 23:41:20 +0530

    Super class

---
+++ JV12_Inheritance/JV01_Vehicles.java
@@ -0,0 +1,6 @@
+class JV01_Vehicles {
+  protected String brand = "Ford";        // Vehicle attribute
+  public void honk() {                    // Vehicle method
+    System.out.println("Tuut, tuut!");
+  }
+}
