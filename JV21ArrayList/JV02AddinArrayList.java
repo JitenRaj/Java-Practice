bd7a750dc424735af749d2ac95ff4d69d4539567
ji10raj <ji10raj@gmail.com>
2022-01-06 19:53:22 +0530

    Adding Element in ArrayList

---
+++ JV21ArrayList/JV02AddinArrayList.java
@@ -0,0 +1,15 @@
+//package JV21ArrayList;
+
+import java.util.ArrayList;
+
+public class JV02AddinArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+
+        System.out.println(arrayList);
+    }
+}
