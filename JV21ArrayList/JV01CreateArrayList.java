04236c7543c349ac3e72a5f77bb2f4cdc601ac4e
ji10raj <ji10raj@gmail.com>
2022-01-06 19:52:52 +0530

    Create ArrayList

---
+++ JV21ArrayList/JV01CreateArrayList.java
@@ -0,0 +1,15 @@
+//package JV21ArrayList;
+
+import java.util.ArrayList;
+
+/**
+ * JV01ArrayList
+ */
+public class JV01CreateArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrList = new ArrayList<>();
+
+        System.out.println(arrList);
+    }
+}
