96de6b5feafdc1e0f987ce224834034e8d3d2ee4
ji10raj <ji10raj@gmail.com>
2022-01-09 22:18:21 +0530

    Creating LinkedList

---
+++ JV22LinkedList/JV01CreateLinkedList.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+/**
+ * JV01CreateLinkedList
+ */
+public class JV01CreateLinkedList {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        System.out.println(list);
+    }
+}
